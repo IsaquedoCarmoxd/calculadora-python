4f5f65d98e6ed32fcb03e484d2358e482f03ddde
Isaque do Carmo <56602702+IsaquedoCarmoxd@users.noreply.github.com>
2021-11-09 13:17:52 -0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+from calculator_factory import make_root,make_display,make_buttons,make_label
+from calculator_class import Calculator
+def main():
+    root = make_root()
+    display = make_display(root)
+    label = make_label(root)
+    buttons = make_buttons(root)
+    calculator = Calculator(root,label,display, buttons)
+    calculator.start()
+
+if __name__ == "__main__":
+    main()
